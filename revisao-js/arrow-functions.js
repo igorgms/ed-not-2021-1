cc58ef2eadc8f8e336c41da5fde63b33dddd56ad
igorgms <igorgmsf@gmail.com>
2021-02-19 01:20:53 +0000

    (18/02) Arrow functions e objetos

--- revisao-js/arrow-functions.js
+++ revisao-js/arrow-functions.js
@@ -13,3 +13,53 @@ console.log(quadrado(9))
 // Transformando em arrow function
 let quadrado2 = n => n * n;
 console.log(quadrado2(9))
+
+// Função com mais de um parâmetro
+function potencia(b, e) { // b = base, e = expoente
+    return b ** e
+}
+
+console.log("função normal", potencia(2, 8))
+
+// Função equivalente no formato arrow function
+var potencia2 = (b, e) => b ** e
+
+console.log("arrow function", potencia2(2, 8))
+
+// Função sem parâmetro
+function megaSena() {
+    // Esta função retorna um número aleatório entre 0 e 1
+    // Math.random -> retorna um número aleatório entre 0(inclusive) e 1(exclusive)
+    return Math.floor(Math.random() * 60 + 1)
+}
+
+console.log(megaSena(), megaSena(), megaSena(), megaSena(), megaSena(), megaSena())
+
+// Quando a função não tem argumentos, na arrow function é necessário deixar entre parênteses vazios para marcar seu lugar
+const megaSena2 = () => Math.floor(Math.random() * 60 + 1)
+
+for (i = 0; i < 6; i++) {
+    console.log(megaSena2())
+}
+
+
+// Função com mais de uma linha no corpo
+function somaVet(vet) {
+    let soma = 0
+    for (let n of vet) {
+        soma += n
+    }
+    return soma
+}
+console.log("soma vet", somaVet([12, 42, -11, 20, 9, 16]))
+
+// Quando a função tem mais de uma linha de código, na arrow function equivalente retornam as chaves e também a palavra return, caso ela retorne valor
+const somaVet2 = vet => {
+    let soma = 0
+    for (let n of vet) soma += n
+    return soma                 
+}
+
+console.log("somavet2", somaVet2([12, 42, -11, 20, 9, 16]))
+
+// CONCLUSÃO: só vale a pena usar arrow function se a função tiver apenas uma linha
---
+++ revisao-js/objetos.js
@@ -0,0 +1,57 @@
+// Objetos são estruturas que permitem armazenar diversos valores em uma variável, permitindo acessá-los por meio de um nome
+
+// Criação de um objeto vazio
+let vazio1 = {} // Forma moderna
+let vazio2 = new Object() // Forma antiga
+
+// Criando um objeto já com dados
+let pessoa = {
+    nome: "Igor Gomes Firmino",
+    sexo: "M",
+    dataNasc: "08-12-1993",
+    profissao: "Desenvolvedor",
+    // nome da propriedade pode ter espaços ou acento
+    // Nesse caso, o nome da propriedade precisa estar entre aspas
+    'Cidade de Origem': 'Franca/SP',
+    gostos: ['flamengo', 'séries', 'cerveja']
+}
+console.table(pessoa)
+
+
+// Exibindo apenas o valor de uma propriedade
+console.log(pessoa.nome)
+console.log(pessoa.profissao)
+console.log(pessoa.gostos[0])
+console.log(pessoa['Cidade de Origem'])
+console.log(pessoa.gostos)
+
+// A sintaxe dos colchetes sempre funciona
+console.log(pessoa['nome'])
+console.log(pessoa['gostos'][0])
+
+// Outra consequência dos colchetes é a possibilidade de usar nomes de propriedades que estão dentro de variáveis
+let x = 'sexo'
+console.log(pessoa[x])
+x = 'dataNasc'
+console.log(pessoa[x])
+
+// Criando uma nova propriedade para um objeto já existente
+// Basta atribuir um valor a um nome de propriedade ainda não existente
+pessoa.email = "igorgmsf@gmail.com"
+pessoa.celular = "(16) 99395-9629"
+console.log(pessoa.celular)
+
+// Criando um objeto vazio e só depois adicionando propriedades
+let carro = {}
+carro.marca = "Wolkswagen"
+carro.modelo = "Gol"
+carro.ano = 2016
+carro['combustível'] = 'Flex'
+carro.cor = 'Branco'
+
+console.table(carro)
+
+// Excluindo uma propriedade de um objeto
+delete carro.cor
+
+console.log(carro)
