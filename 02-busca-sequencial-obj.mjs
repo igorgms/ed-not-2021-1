5237e20b9ac6a3e1627f606f89c48daea0ffc624
igorgms <igorgmsf@gmail.com>
2021-03-02 01:22:23 +0000

    (01/03) Finalização de busca sequencial

--- 02-busca-sequencial-obj.mjs
+++ 02-busca-sequencial-obj.mjs
@@ -1,8 +1,8 @@
-function buscaSequencial(vetor, valorBusca, fnComp) {
+function buscaSequencial(vetor, fnComp) {
     for(let i = 0; i < vetor.length; i++) {
         // Em vez de fazer a comparação dentro da função de busca,
         // "terceirizamos" essa comparação chamando uma função externa
-        if(fnComp(vetor[i], valorBusca)) return i // Retorna a posição onde o valor de busca foi encontrado
+        if(fnComp(vetor[i])) return i // Retorna a posição onde o valor de busca foi encontrado
     }
     return -1 // Valor de busca não encontrado
 }
@@ -11,9 +11,17 @@ function buscaSequencial(vetor, valorBusca, fnComp) {
 //     return objNome.first_name === valorBusca
 // }
 
-const comparaNome = (objNome, valorBusca) => objNome.first_name === valorBusca
+// const comparaNome = (objNome, valorBusca) => objNome.first_name === valorBusca
 
 import { objNomes } from './includes/vetor-obj-nomes.mjs'
 
-console.log(buscaSequencial(objNomes, 'FAUSTO', comparaNome))
-console.log(buscaSequencial(objNomes, 'IGOR', comparaNome))
+console.log(buscaSequencial(objNomes, obj => obj.first_name === 'FAUSTO'))
+console.log(buscaSequencial(objNomes, obj => obj.first_name === 'MARIA'))
+
+// Primeiro nome da lista que tem mais de 1000 pessoas registradas com ele
+let pos = buscaSequencial(objNomes, obj => obj.frequency_total > 1000)
+console.log(objNomes[pos])
+
+// Primeiro nome masculino da lista com mais de 10000 registros
+pos = buscaSequencial(objNomes, obj => obj.frequency_total > 10000 && obj.classification === "M")
+console.log(objNomes[pos])
